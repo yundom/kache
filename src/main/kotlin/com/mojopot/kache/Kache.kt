3733bccca4c68f147cf5b5bbcdd6c2e4c52e77b6
Dennis <yundom@gmail.com>
2018-01-13 14:32:02 +0800

    Create kache interface and test code.

---
+++ src/main/kotlin/com/mojopot/kache/Kache.kt
@@ -0,0 +1,16 @@
+package com.mojopot.kache
+
+interface Kache<K, V> {
+
+    fun get(key: K): V
+
+    fun put(key: K, value: V): V
+
+    fun remove(key: K): V
+
+    fun clear()
+
+    fun getMaxMemorySize(): Int
+
+    fun getMemorySize(): Int
+}
