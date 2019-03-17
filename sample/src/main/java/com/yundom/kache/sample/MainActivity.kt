6402b7649adf23abe8a78bd809ecec41d17f520d
Dennis <dennis.hsieh@agoda.com>
2019-03-17 16:18:50 +0700

    Import kache 1.0.4

--- sample/src/main/java/com/yundom/kache/sample/MainActivity.kt
+++ sample/src/main/java/com/yundom/kache/sample/MainActivity.kt
@@ -2,11 +2,17 @@ package com.yundom.kache.sample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.yundom.kache.Builder
+import com.yundom.kache.Kache
+import com.yundom.kache.config.LRU
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val cache: Kache<Int, Int> = Builder.build {
+            policy = LRU
+        }
     }
 }
